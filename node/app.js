bff5d538df00ae39b2b0b84d6b4be0799e784bdd
Nikita Balashov <nik.blash@gmail.com>
2021-08-02 01:10:31 +0300

    Jest

--- node/app.js
+++ node/app.js
@@ -1,11 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+module.exports = app;
