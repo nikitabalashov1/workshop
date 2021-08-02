d8385b797c02e186da6bf00b4665546f57b7c865
nikitabalashov1 <34027222+nikitabalashov1@users.noreply.github.com>
2021-08-02 13:53:00 +0300

    Update app.test.js

--- node/app.test.js
+++ node/app.test.js
@@ -6,7 +6,7 @@ describe("Test the root path", () => {
     request(app)
       .get("/")
       .then(response => {
-        expect(response.statusCode).toBe(200);
+        expect(response.statusCode).toBe(300);
         done();
       });
   });
